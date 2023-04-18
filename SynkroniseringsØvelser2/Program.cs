9e39ea2d08739685fbcf4dece4b47d5611b7fe67
Simon <Simon@DESKTOP-AFA0UEO>
2023-04-18 10:32:17 +0200

    Add project files.

---
+++ SynkroniseringsØvelser2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SynkroniseringsØvelser2
+{
+    internal class Program
+    {
+        static int counter = 0;
+        static readonly object obj = new object();
+
+        public void Stjerner()
+        {
+            Monitor.Enter(obj);
+            try
+            {
+                counter += 60;
+                Console.WriteLine($"******************************************************************************* {counter}\n");
+            }
+            finally
+            {
+                Monitor.Exit(obj);
+            }
+            Thread.Sleep(50);
+        }
+        public void HaveLåge()
+        {
+            Monitor.Enter(obj);
+            try
+            {
+                counter += 60;
+                Console.WriteLine($"############################################################################### {counter}\n");
+            }
+            finally
+            {
+                Monitor.Exit(obj);
+            }
+            Thread.Sleep(50);
+        }
+
+        static void Main(string[] args)
+        {
+            Program pg = new Program();
+            for (int i = 0; i < 2; i++)
+            {
+                Thread t1 = new Thread(pg.Stjerner);
+                t1.Start();
+               
+                Thread t2 = new Thread(pg.HaveLåge);
+                t2.Start();
+            }
+            Console.Read();
+        }
+    }
+}
